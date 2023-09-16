0750f10e1fd39417129548ead16113cd6ecc19b8
langat <wiztyping@gmail.com>
2023-09-16 11:48:21 +0300

    Committing code from data science lessons

---
+++ lesson2b.py
@@ -0,0 +1,9 @@
+#2. DATA TYPES:Used to define the type of data in a variable
+#Because it specifies the operations to be done
+#Categorised into two
+#1. Primary Data Types (Strings(text), Integers(whole numbers),
+#Floating Points(fractions), Boolean(True/ False))
+#2. Secondary: Collections -> Lists[], Tuple(), Dictionary{}, Sets{}
+
+#Strings: Textual Data
+
