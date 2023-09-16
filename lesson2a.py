5a868675b8b9b79bdbe6969f049878badbcf38ea
langat <wiztyping@gmail.com>
2023-09-16 11:53:35 +0300

    Committing 2d

---
+++ lesson2a.py
@@ -0,0 +1,42 @@
+print ("Hello World")
+print ("Welcome to Python for Data Science")
+
+#Ignored
+#Variables: Stored Values
+course = "Artificial Intelligence"
+cost = 2000
+
+print (course, cost)
+print(f"The {course} course will cost you KES {cost}")
+
+#operators: Symbols or keywords used to perform operations
+#Arithmetic: +, -, /, *
+#Comparison operators: (<, >, <=, >=, ==, !=)
+#They are used to check the relationship between variables/ values(they form conditions) - alt + Z for line break, they return two instances(True or False)
+
+variable1 =  100
+variable2 = 200
+print(variable1 > variable2)
+print(variable1 < variable2)
+print(variable1 <= variable2)
+print(variable1 >= variable2)
+print(variable1 == variable2)
+print (variable1 != variable2)
+
+#Simple Login System
+registered_password = "user@123"
+user_password = "123"
+
+registered_username = "admin"
+received_username = "admin"
+print(registered_password == user_password)
+print(registered_username == received_username)
+
+#Logical Operators: and, or, not
+#Used to check relationship between condition
+#and: Return True only when both conditions return True, otherwise False
+#or : Returns True, when atleast one condition is True, but returns False when both are false
+
+print(registered_username == received_username and not registered_password == user_password)
+
+
--- lesson2b.py
+++ lesson2b.py
@@ -6,4 +6,20 @@
 #2. Secondary: Collections -> Lists[], Tuple(), Dictionary{}, Sets{}
 
 #Strings: Textual Data
+#Enclosed inside double/ single
+#sequence of Characters
 
+string1 = "I love" + " "
+string2 = "Programming"
+
+#1. Concatenation(merge/ join strings) -> +
+whole_string = string1 + string2
+print(whole_string)
+
+#2. Length
+#len(): Returns the number of characters in a string
+language = "Kotlin"
+print(len(language))
+
+#type(): Determine datatype
+print(type(language))
