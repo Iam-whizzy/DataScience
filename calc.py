9df81678f0bce86fb6f663d28ed8826aad57146d
Langat <wiztyping@gmail.com>
2023-09-19 23:57:00 +0300

    Committing with seaborn error to resolve later

---
+++ calc.py
@@ -0,0 +1,10 @@
+#to be imported
+
+def add(a,b):
+ 
+    print(a+b)
+    
+def substract(a,b):
+    
+    print(a-b)
+
--- ppa.py
+++ ppa.py
@@ -1,5 +1,5 @@
 # DICTIONARIES
-
+import calc
 person = {
     'firstname': 'John',
     'lastname' : 'Doe' ,
@@ -10,4 +10,15 @@ person = {
 }
 print(person['age'])
 print(person['phone_numbers'])
+
+for counter in range(5):
+    print(counter)
+
+tensors = [[1,2], 3, [4,[5,6,[7,8]]]]
+for tensors in tensors:
+    print (tensors)
+    
+print (calc.add(10, 2))
+print (calc.substract(10, 2))
+
 
