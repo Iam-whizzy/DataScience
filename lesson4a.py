ae42fea9fa58968ce3251c2440b5ea5acad705b4
langat <wiztyping@gmail.com>
2023-09-26 10:30:41 +0300

    A bit of pandas and interactive

---
+++ lesson4a.py
@@ -0,0 +1,14 @@
+#Control Structures
+#Conditional Control Structures
+#Iterative
+
+#Control Structures: It determines the order in which programs are executed
+# a) Skip some programs while executing other -> condition
+# b) Repeat the execution of some codes -> looping
+# By default: Python performs a sequential control structure, codes don't have indentations
+
+print("Python")
+print("Python")
+print("Python")
+print("Python")
+# c) Control Structures: Codes are executed based on conditions -> if, if...else, if..elseif..else
